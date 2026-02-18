9f2eb5b71b44638dfc9df1ac26d7b5c2a02a7dec
Sakshi <portgasdyamato@users.noreply.github.com>
2026-02-18 23:12:11 +0530

    Final name polish in header

--- components/header.tsx
+++ components/header.tsx
@@ -82,7 +82,7 @@ export default function Header() {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
-          Sakshi 
+          Sakshi Agrahari
         </motion.button>
         
         {/* Desktop Navigation */}
